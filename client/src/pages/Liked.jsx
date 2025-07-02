109ceace8c75d1f4402e29f89ed1307213f22f08
3RaghavK3 <33raghavk33@gmail.com>
2025-07-02 19:14:55 +0530

    :changed capitalisation

--- client/src/pages/Liked.jsx
+++ client/src/pages/Liked.jsx
@@ -1,4 +1,4 @@
-import { MainLayout } from '../layout/Mainlayout';
+import { MainLayout } from '../layout/MainLayout';
 import { SongCards } from '../components/SongsPage/songCards';
 import { LikedContext } from '../context/LikedContext';
 import { useContext } from 'react';
